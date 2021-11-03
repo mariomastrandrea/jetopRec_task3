3d699559be9b59e4e9d54fe58695716267513ace
mariomastrandrea <mariomastrandrea.mate@gmail.com>
2021-11-04 00:20:05 +0100

    initial commit

---
+++ task3_dotnetProject/Services/OWMforecastService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Http;
+
+namespace task3_dotnetProject.Services
+{
+    public class OWMforecastService
+    {
+        private readonly HttpClient OMWclient;
+
+        public OWMforecastService(HttpClient OMWclient)
+        {
+            this.OMWclient = OMWclient;
+        }
+
+        //
+
+    }
+}
