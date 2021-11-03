03edf801cbd015e88f65397ed361cd8bf50567d4
mariomastrandrea <mariomastrandrea.mate@gmail.com>
2021-11-04 00:13:46 +0100

    wefg

---
+++ task3_dotnetProject/Controllers/WeatherController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using task3_dotnetProject.Services;
+
+namespace task3_dotnetProject.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherController : ControllerBase
+    {
+        private readonly OWMforecastService OWMservice;
+
+
+        public WeatherController(OWMforecastService OWMservice)
+        {
+            this.OWMservice = OWMservice;
+        }
+
+        [HttpGet]
+        public Task<ActionResult<string>> GetAsync(string city, string date)
+        {
+            return null;
+        }
+    }
+}
